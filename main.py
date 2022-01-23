b2b8a9da8f61ab8a64cac7796eeeeafe36555467
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-23 04:16:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#Write a python functiopn to reevrse a string . Import the module to reverse a string input by the user 
+import Reverse
+str1=input("Enter String     ")
+print("Reversed String is:  ",Reverse.string_reverse(str1))
